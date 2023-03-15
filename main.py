925932fe4a7159d07ed90e498c78437667abd73e
woosh1108 <woosh1108@naver.com>
2023-03-15 13:28:43 +0900

    Master 2

--- main.py
+++ main.py
@@ -11,12 +11,19 @@ dst1, dst2, dst3 = None, None, None # 영상처리한 결과
 
 
 
-
 ## 메인 코드부 96
 src = cv2.imread('c:/images/picture18.jpg') # 이미지 읽기
- 
+
+dst1 = grayScale(src)
+dst2 = cannyEdge(src)
+
+
+
+
+cv2.imshow('Src', src) #  화면 출력
+cv2.imshow('Dst1', dst1) #  화면 출력
+cv2.imshow('Dst2', dst2) #  화면 출력
 
 ## 마무리
-cv2.imshow('Src', src) #  화면 출력
 cv2.waitKey(0)
 cv2.destoryAllWindows()
