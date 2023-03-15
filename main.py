20155a7f2929b670637394b4adc7e19915f5819f
woosh1108 <woosh1108@naver.com>
2023-03-15 13:24:22 +0900

    Master 1 영상처리 기본 틀 완성

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+
+## 함수 선언부
+
+
+
+
+## 전역 변수부
+src = None # 원본 이미지
+dst1, dst2, dst3 = None, None, None # 영상처리한 결과
+
+
+
+
+## 메인 코드부 96
+src = cv2.imread('c:/images/picture18.jpg') # 이미지 읽기
+ 
+
+## 마무리
+cv2.imshow('Src', src) #  화면 출력
+cv2.waitKey(0)
+cv2.destoryAllWindows()
